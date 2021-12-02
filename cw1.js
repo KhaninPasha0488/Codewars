2de209db7fd7ac8afa7d4bec1eb4cb1211605059
KhaninPasha0488 <KhaninPasha0488>
2021-12-02 16:27:32 +0300

    02-12 1 Задача

--- cw1.js
+++ cw1.js
@@ -1,4 +1,11 @@
-1
-2
-3
-4
+function multiply(number){
+   if (number >= 0){
+let a = number * (5 ** number.toString().length);
+     return a
+     }else{
+       let a = number * (5 ** 1 );
+     return a
+       
+     }
+}
+
