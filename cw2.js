b7baeb686fff0c7e5353e2ad2f1e173928ca643f
KhaninPasha0488 <KhaninPasha0488>
2021-12-08 21:30:42 +0300

    cw 08-12 001

--- cw2.js
+++ cw2.js
@@ -26,3 +26,14 @@ var stringToNumber = function(str){
   // put your code here
   return Number(str)
 };
+function yearDays(year) {
+  if (year % 4 !== 0) {
+    return year + ' has 365 days';
+  } else if (year % 100 !== 0) {
+    return year + ' has 366 days';
+  } else if (year % 400 !== 0) {
+    return year + ' has 365 days';
+  } else {
+    return year + ' has 366 days';
+  }
+}
