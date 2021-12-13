766b79429fbfbb44ab9af27ef450a75ba696710b
KhaninPasha0488 <KhaninPasha0488>
2021-12-13 11:27:33 +0300

    13-12cw 001

---
+++ cw5.js
@@ -0,0 +1,34 @@
+// 1 Simple multiplication
+const  simpleMultiplication=(n) =>n%2==0?n*8:n*9
+   
+// Area or Perimeter
+const areaOrPerimeter = function(l , w) {
+   return l==w?l*w:(l+w)*2
+ };
+// Grasshopper - Messi goals function
+ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+   return laLigaGoals + copaDelReyGoals+championsLeagueGoals
+ }
+// Is n divisible by x and y?
+ const isDivisible =(n, x, y) => n % x === 0 && n % y === 0 ? true :false
+
+// Function 3 - multiplying two numbers
+ const multiply = (a,b) => a*b
+//  Return Negative
+const makeNegative =(n) =>n>0?n*(-1):n
+
+// Grasshopper - Variable Assignment Debug
+var a = "dev"
+var b = "Lab"
+
+var name =a + b
+
+// Grasshopper - Basic Function Fixer
+function addFive(num) {
+   return  num + 5
+  
+ }
+//  Grasshopper - Terminal game move function
+function move (p, r) {
+   return p +( r*2)
+  }
