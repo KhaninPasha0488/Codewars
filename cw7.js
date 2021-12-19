74a2bfeb31d7ea6cec4db6acec5bea19ac4206e4
KhaninPasha0488 <KhaninPasha0488>
2021-12-19 20:38:38 +0300

    19-12-001

--- cw7.js
+++ cw7.js
@@ -47,3 +47,15 @@ function stringToArray(string){
 
   const greet =(n, o) => n ==o?'Hello boss':'Hello guest'
   
+  function finalGrade (e, p) {
+    if(e>90  || p>10){
+      return 100
+    } else if(  e > 75 &&  p >= 5){
+      return 90
+    }else if( e > 50 && p >=2){
+      return 75
+    }else {
+      return 0
+    }
+    
+    
